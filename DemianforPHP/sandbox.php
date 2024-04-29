8170ca8dc407d0cedf554906623a666a14486273
jsilverhead <109525905+jsilverhead@users.noreply.github.com>
2024-04-29 20:17:13 +0300

    updated info

--- DemianforPHP/sandbox.php
+++ DemianforPHP/sandbox.php
@@ -1,9 +1,45 @@
 <?php
 
-$arr = [2, 3, 7, 9];
+class User
+{
+	protected string $name;
+	protected int $id;
+	protected string $email;
+	protected string $password;
 
-$result = array_map(function ($item) {
-	return $item + 5;
-}, $arr);
+	public function __construct($name, $id, $email, $password)
+	{
+		$this->name = $name;
+		$this->id = $id;
+		$this->email = $email;
+		$this->password = $password;
+	}
 
-print_r($result);
+	public function getInfo()
+	{
+		$info = "name: $this->name, id: $this->id, email: $this->email, password: $this->password";
+
+		return $info;
+	}
+}
+
+class Admin extends User
+{
+	protected string $type;
+
+	public function __construct($name, $id, $email, $password, $type)
+	{
+		parent::__construct($name, $id, $email, $password);
+		$this->type = $type;
+	}
+
+	public function getInfo()
+	{
+		$info = parent::getInfo();
+		$info .= ", type: $this->type";
+	}
+}
+
+$administrator = new Admin($name = "John", $id = 22, $email = "lovedogs@gmail.com", $password = "12345678", $type = "admin");
+
+var_dump($administrator);
