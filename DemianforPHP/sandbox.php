f2915372cd8d6a1d17131840df966dee94e616e0
jsilverhead <109525905+jsilverhead@users.noreply.github.com>
2024-04-29 21:47:52 +0300

    more OOP

--- DemianforPHP/sandbox.php
+++ DemianforPHP/sandbox.php
@@ -1,45 +1,20 @@
 <?php
 
-class User
+abstract class Sandbox
 {
-	protected string $name;
-	protected int $id;
-	protected string $email;
-	protected string $password;
+	public int $id;
 
-	public function __construct($name, $id, $email, $password)
+	abstract function getId();
+}
+
+class box extends Sandbox
+{
+	public function getId()
 	{
-		$this->name = $name;
-		$this->id = $id;
-		$this->email = $email;
-		$this->password = $password;
-	}
-
-	public function getInfo()
-	{
-		$info = "name: $this->name, id: $this->id, email: $this->email, password: $this->password";
-
-		return $info;
+		return $this->id;
 	}
 }
 
-class Admin extends User
-{
-	protected string $type;
-
-	public function __construct($name, $id, $email, $password, $type)
-	{
-		parent::__construct($name, $id, $email, $password);
-		$this->type = $type;
-	}
-
-	public function getInfo()
-	{
-		$info = parent::getInfo();
-		$info .= ", type: $this->type";
-	}
-}
-
-$administrator = new Admin($name = "John", $id = 22, $email = "lovedogs@gmail.com", $password = "12345678", $type = "admin");
-
-var_dump($administrator);
+$box1 = new box;
+$box1->id = 22;
+echo $box1->getId();
