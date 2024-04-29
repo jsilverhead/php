8a52e63641f8333aeefd71338ec1a7e0adac0792
jsilverhead <109525905+jsilverhead@users.noreply.github.com>
2024-04-29 12:48:41 +0300

    new info

--- DemianforPHP/sandbox.php
+++ DemianforPHP/sandbox.php
@@ -1,3 +1,12 @@
 <?php
 
-print_r($_SERVER);
+function evenNoEven(array $array, int $value)
+{
+	$result = array_filter($array, function ($item) use ($value) {
+		return $item % $value != 0;
+	});
+
+	return array_values($result);
+}
+
+print_r(evenNoEven([22, 9, 11, 3, 44, 2], 2));
