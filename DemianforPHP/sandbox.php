7a047a99790e101ae3ae5f11ad57406da81adea3
jsilverhead <109525905+jsilverhead@users.noreply.github.com>
2024-05-01 15:20:36 +0300

    magic OOP methods

--- DemianforPHP/sandbox.php
+++ DemianforPHP/sandbox.php
@@ -1,33 +1,15 @@
 <?php
-trait noNegative
+
+class Sandbox
 {
-	public function checkNehative($array)
+
+
+	public function __invoke($data)
 	{
-		return array_filter($array, function ($item) {
-			return $item >= 0;
-		});
+		return $data + 1;
 	}
 }
 
-trait notEven
-{
-	public function checkEven($array)
-	{
-		return array_filter($array, function ($item) {
-			return $item % 2 != 0;
-		});
-	}
-}
+$sandbox = new Sandbox;
 
-class checkArray
-{
-	use noNegative, notEven;
-}
-
-$array = [0, 22, -1, 1, 31, 42, 99, 69, 77, -100];
-$checkArray = new checkArray;
-
-$array = $checkArray->checkNehative($array);
-$array = $checkArray->checkEven($array);
-
-print_r($array);
+echo $sandbox(5);
