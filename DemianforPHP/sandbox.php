07f5da2644d610107ceb152822c0f2408c45c6ba
jsilverhead <109525905+jsilverhead@users.noreply.github.com>
2024-05-02 22:23:13 +0300

    added trycatch

--- DemianforPHP/sandbox.php
+++ DemianforPHP/sandbox.php
@@ -1,15 +1,28 @@
 <?php
 
-class Sandbox
+set_exception_handler('errorMessage');
+
+function findWord($words)
 {
+	$wordCount = 0;
 
-
-	public function __invoke($data)
-	{
-		return $data + 1;
+	foreach ($words as $word) {
+		if (gettype($word) != "string") {
+			throw new Exception(" $word Isn't a word!");
+		}
+		$wordCount++;
 	}
+	echo "$wordCount \n";
 }
 
-$sandbox = new Sandbox;
+try {
+	findWord(["It's", "Good", "To", "See", "Ya"]);
+	findWord(["Billy", "Jean", "Is", 11, "years"]);
+} finally {
+	echo "Completed search. \n";
+}
 
-echo $sandbox(5);
+function errorMessage(Throwable $exception)
+{
+	echo "Exception found:" . $exception->getMessage();
+}
