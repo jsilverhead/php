078b065a42f88ecc931a08b8b6a72bc6edaa8294
jsilverhead <109525905+jsilverhead@users.noreply.github.com>
2024-05-06 21:11:52 +0300

    fixes

--- DemianforPHP/sandbox/sandbox.php
+++ DemianforPHP/sandbox/sandbox.php
@@ -1,11 +1,5 @@
 <?php
 
-function countNums($a, $b)
-{
-    return $a + $b;
-}
+$array = [1, 6, "mother"];
 
-echo countNums(...[2, 5, 9]) . "\n"; // Пересчитает 2, 5
-
-$getSum = [8, 8];
-echo countNums(...$getSum) . "\n"; // Пересчитает 8 и 8
+echo join('.', $array);
