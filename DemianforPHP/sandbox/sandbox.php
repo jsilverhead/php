2f48ffa99ce3c248e93d3e2c9bc828efd785067c
jsilverhead <109525905+jsilverhead@users.noreply.github.com>
2024-05-07 22:33:35 +0300

    updates

--- DemianforPHP/sandbox/sandbox.php
+++ DemianforPHP/sandbox/sandbox.php
@@ -1,5 +1,31 @@
 <?php
 
-$points = [1190, 1, 6, 1];
+class SandboxException extends Exception
+{
+    public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 
-echo array_sum($points);
+    public function __toString(): string
+    {
+        return "Exception found in " . __CLASS__ . ": " . $this->code . $this->message;
+    }
+
+    public function printMessage(): string
+    {
+        return __CLASS__ . " found. " . "Code: " . ($this->code ? $this->code : "no code") . ". Message: " . ($this->message ? $this->message : "no message") . "\n";
+    }
+}
+
+try {
+    if (!isset($statement)) {
+        throw new SandboxException("Statement isn't set", 400);
+    }
+} catch (SandboxException $e) {
+    echo $e->printMessage();
+} catch (Exception $e) {
+    echo $e->getMessage();
+} finally {
+    echo "Operation closed";
+}
