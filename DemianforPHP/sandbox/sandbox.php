40428af643f116ea8deffd90174e19166f01d778
jsilverhead <109525905+jsilverhead@users.noreply.github.com>
2024-05-12 18:41:43 +0300

    OOP updates

--- DemianforPHP/sandbox/sandbox.php
+++ DemianforPHP/sandbox/sandbox.php
@@ -1,31 +1,12 @@
 <?php
 
-class SandboxException extends Exception
+class Sandbox
 {
-    public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
+    public int $value = 42;
 
-    public function __toString(): string
-    {
-        return "Exception found in " . __CLASS__ . ": " . $this->code . $this->message;
-    }
-
-    public function printMessage(): string
-    {
-        return __CLASS__ . " found. " . "Code: " . ($this->code ? $this->code : "no code") . ". Message: " . ($this->message ? $this->message : "no message") . "\n";
-    }
 }
 
-try {
-    if (!isset($statement)) {
-        throw new SandboxException("Statement isn't set", 400);
-    }
-} catch (SandboxException $e) {
-    echo $e->printMessage();
-} catch (Exception $e) {
-    echo $e->getMessage();
-} finally {
-    echo "Operation closed";
+class box extends Sandbox
+{
+    private int $value = 42;
 }
