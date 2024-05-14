09e4fed2962f0ef15e6dd81005049330433d4c6e
jsilverhead <109525905+jsilverhead@users.noreply.github.com>
2024-05-14 22:13:23 +0300

    updates: OOP

--- DemianforPHP/sandbox/sandbox.php
+++ DemianforPHP/sandbox/sandbox.php
@@ -1,8 +1,16 @@
 <?php
 
-namespace Example;
-
 class Sandbox
 {
-    static int $x = new Foo;
+    public string $name = 'John';
+    private string $fullname = 'Week';
+    protected int $age = 40;
+
+    public function __debugInfo()
+    {
+        echo "what do you wanna know? It's protected.";
+    }
 }
+
+$sandbox = new Sandbox;
+var_dump($sandbox);
