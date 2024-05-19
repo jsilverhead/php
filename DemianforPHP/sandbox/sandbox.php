91d9be94db8fe14f97cc4784308053c958b1aebb
jsilverhead <109525905+jsilverhead@users.noreply.github.com>
2024-05-19 10:06:27 +0300

    generators updated

--- DemianforPHP/sandbox/sandbox.php
+++ DemianforPHP/sandbox/sandbox.php
@@ -1,15 +1,34 @@
 <?php
 
-function gen_one_to_three()
+function getNumbers()
 {
-    for ($i = 1; $i <= 3; $i++) {
-        // Обратите внимание, что $i сохраняет своё значение между вызовами.
-        yield $i;
-    }
+    yield 1;
+    yield from [2, 3];
+    yield from fourfive();
+    yield from new ArrayIterator([6, 7]);
+    yield 8;
+    return yield from nineten();
 }
 
-$generator = gen_one_to_three();
-
-foreach ($generator as $value) {
-    echo $value;
+function fourfive()
+{
+    yield 4;
+    yield from five();
 }
+
+function five()
+{
+    yield 5;
+}
+
+function nineten()
+{
+    yield 9;
+    return 10;
+}
+
+$generator = getNumbers();
+foreach ($generator as $num) {
+    echo "$num "; // выдаст 1 2 3 4 5 6 7 8 9
+}
+echo $generator->getReturn(); // вернёт последний return 10;
